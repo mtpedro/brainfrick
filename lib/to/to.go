6303ba1abd039f7b08ddebd8ed6070e1dfc3fbfb
hugo <hugo@moniker.net>
2022-09-25 15:03:41 +1000

    replaced "if" with "case"

--- lib/to/to.go
+++ lib/to/to.go
@@ -5,81 +5,81 @@ import (
 )
 
 type Program struct {
-    size         int
+    size int
     instructions []byte
-    at           int
+    at int
 }
 
 func check(e error) {
     if e != nil {
-        fmt.Println(e)
-        panic(e)
+        fmt.Println(e);
+        panic(e);
     }
 }
 
 func Execute(code string) string {
-    var program = new(Program)
-    program.size = 30000
-    program.instructions = make([]byte, program.size, program.size)
-    program.at = 0
+    var program = new(Program);
+    program.size = 30000;
+    program.instructions = make([]byte, program.size, program.size);
+    program.at = 0;
 
-	return executeWith(program, code)
+	return executeWith(program, code);
 }
 
 func executeWith(program *Program, code string) string {
-    var loopStart = -1
-    var loopEnd = -1
-    var ignore = 0
-    var skipClosingLoop = 0
+    var loopStart = -1;
+    var loopEnd = -1;
+    var ignore = 0;
+    var skipClosingLoop = 0;
 	var text string;
 
     for pos, char := range code {
         if ignore == 1 {
             if char == '[' {
-                skipClosingLoop += 1
+                skipClosingLoop += 1;
             } else if char == ']' {
                 if skipClosingLoop != 0 {
-                    skipClosingLoop -= 1
-                    continue
+                    skipClosingLoop -= 1;
+                    continue;
                 }
 
-                loopEnd = pos
-                ignore = 0
+                loopEnd = pos;
+                ignore = 0;
                 if loopStart == loopEnd {
-                    loopStart = -1
-                    loopEnd = -1
-                    continue
+                    loopStart = -1;
+                    loopEnd = -1;
+                    continue;
                 }
-                loop := code[loopStart:loopEnd]
+                loop := code[loopStart:loopEnd];
                 for program.instructions[program.at] > 0 {
-                    executeWith(program, loop)
+                    executeWith(program, loop);
                 }
             }
-            continue
+            continue;
         }
-
-        if char == '+' {
-            program.instructions[program.at] += 1
-        } else if char == '-' {
-            program.instructions[program.at] -= 1
-        } else if char == '>' {
-            if program.at == program.size-1 {
-                program.at = 0
-            } else {
-                program.at += 1
-            }
-        } else if char == '<' {
-            if program.at == 0 {
-                program.at = program.size - 1
-            } else {
-                program.at -= 1
-            }
-        } else if char == '.' {
-            text = fmt.Sprintf("%v%c", text, rune(program.instructions[program.at]))
-        } else if char == '[' {
-            loopStart = pos + 1
-            ignore = 1
+        switch char {
+            case '+':
+                program.instructions[program.at] += 1;
+            case '-':
+                program.instructions[program.at] -= 1;
+            case '>':
+                if program.at == program.size-1 {
+                    program.at = 0;
+                } else {
+                    program.at += 1;
+                }
+            case '<':
+                if program.at == 0 {
+                    program.at = program.size - 1;
+                } else {
+                    program.at -= 1;
+                }
+            case '.':
+                text = fmt.Sprintf("%v%c", text, rune(program.instructions[program.at]));
+            case '[':
+                loopStart = pos + 1;
+                ignore = 1;
         }
     }
-	return text
+	return text;
 }
